50b487a527109048ced89e1b1f1a2ba94a3c39a9
Ondrej Bouda <bouda@edookit.com>
2024-06-15 01:37:35 +0200

    server: add tests for getting router check-in status

---
+++ server/tests/Application/Actions/V1/Game/GetRouterCheckInStatusActionTest.php
@@ -0,0 +1,112 @@
+<?php
+declare(strict_types=1);
+namespace Tests\Application\Actions\V1\Game;
+
+use Tests\TestCase;
+
+class GetRouterCheckInStatusActionTest extends TestCase
+{
+    public function testAction(): void
+    {
+        $request = $this->createRequest('GET', '/v1/game/round/4/checkin');
+        $response = $this->app->handle($request);
+
+        $expectedJson = <<<'JSON'
+{
+    "roundId": 4,
+    "checkedIn": [
+        {
+            "router": "B",
+            "teamId": "A",
+            "card": "A002"
+        },
+        {
+            "router": "E",
+            "teamId": "A",
+            "card": "A005"
+        }
+    ],
+    "missing": [
+        {
+            "router": "A",
+            "teamId": "A"
+        },
+        {
+            "router": "A",
+            "teamId": "B"
+        },
+        {
+            "router": "B",
+            "teamId": "B"
+        },
+        {
+            "router": "C",
+            "teamId": "A"
+        },
+        {
+            "router": "C",
+            "teamId": "B"
+        },
+        {
+            "router": "D",
+            "teamId": "A"
+        },
+        {
+            "router": "D",
+            "teamId": "B"
+        },
+        {
+            "router": "E",
+            "teamId": "B"
+        },
+        {
+            "router": "F",
+            "teamId": "A"
+        },
+        {
+            "router": "F",
+            "teamId": "B"
+        },
+        {
+            "router": "G",
+            "teamId": "A"
+        },
+        {
+            "router": "G",
+            "teamId": "B"
+        },
+        {
+            "router": "H",
+            "teamId": "A"
+        },
+        {
+            "router": "H",
+            "teamId": "B"
+        },
+        {
+            "router": "I",
+            "teamId": "A"
+        },
+        {
+            "router": "I",
+            "teamId": "B"
+        },
+        {
+            "router": "J",
+            "teamId": "A"
+        },
+        {
+            "router": "J",
+            "teamId": "B"
+        }
+    ]
+}
+JSON;        
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertSame(
+            str_replace("\r\n", "\n", $expectedJson),
+            (string)$response->getBody()
+        );
+    }
+}
