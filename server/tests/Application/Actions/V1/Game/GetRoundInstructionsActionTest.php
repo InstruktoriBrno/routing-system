185564a65941f7cf160d06f40944fa8abd989c30
Ondrej Bouda <bouda@edookit.com>
2024-06-17 00:16:45 +0200

    server: fix tests for GetRoundInstactionsAction to include packet type

--- server/tests/Application/Actions/V1/Game/GetRoundInstructionsActionTest.php
+++ server/tests/Application/Actions/V1/Game/GetRoundInstructionsActionTest.php
@@ -14,14 +14,14 @@ class GetRoundInstructionsActionTest extends TestCase
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertSame(
             <<<'TXT'
-001: A, min 0
-002: B, min 0
-003: C, min 0
-011: A, min 0 sec 30
-010: A, min 1
-030: B, min 1
-012: A, min 1 sec 30
-020: B, min 3
+001: A, min 0, locator
+002: B, min 0, locator
+003: C, min 0, locator
+011: A, min 0 sec 30, standard
+010: A, min 1, standard
+030: B, min 1, standard
+012: A, min 1 sec 30, standard
+020: B, min 3, standard
 
 TXT
             ,
@@ -35,25 +35,25 @@ TXT
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertSame(
             <<<'TXT'
-001: A, min 0
-002: B, min 0
-003: C, min 0
-004: D, min 0
-005: E, min 0
-006: F, min 0
-007: G, min 0
-008: H, min 0
-009: I, min 0
-010: J, min 0
-020: A, min 1
-021: A, min 2
-022: A, min 3
-023: A, min 4
-024: A, min 5
-025: A, min 6
-026: A, min 7
-027: A, min 8
-028: A, min 9
+001: A, min 0, locator
+002: B, min 0, locator
+003: C, min 0, locator
+004: D, min 0, locator
+005: E, min 0, locator
+006: F, min 0, locator
+007: G, min 0, locator
+008: H, min 0, locator
+009: I, min 0, locator
+010: J, min 0, locator
+020: A, min 1, standard
+021: A, min 2, standard
+022: A, min 3, standard
+023: A, min 4, standard
+024: A, min 5, standard
+025: A, min 6, standard
+026: A, min 7, standard
+027: A, min 8, standard
+028: A, min 9, standard
 
 TXT
             ,
