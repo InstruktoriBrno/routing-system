9b3ecd985db5e259bd61c867e46f4951f1624345
Ondrej Bouda <bouda@edookit.com>
2024-07-15 22:30:03 +0200

    server: improve setup-game command: -vv to pretty-print, game_round existence check, --start option

--- server/src/Console/SetupGameCommand.php
+++ server/src/Console/SetupGameCommand.php
@@ -10,6 +10,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class SetupGameCommand extends CommandBase
 {
     private const ARG_ROUND_IDENT = 'roundId';
+    private const OPT_START = 'start';
+    private const SLEEP_BEFORE_START = 3;
 
     protected function define(): void
     {
@@ -27,6 +29,13 @@ final class SetupGameCommand extends CommandBase
             Game round API identifier. This will usually be the game_round.id.
             TEXT
         );
+
+        $this->addOption(
+            self::OPT_START,
+            null,
+            null,
+            'After setup succeeds, wait 3 seconds and start the game.'
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -37,13 +46,39 @@ final class SetupGameCommand extends CommandBase
             'SELECT * FROM game_round WHERE api_ident = %int',
             $roundIdent
         );
+        if ($round === null) {
+            $output->writeln(sprintf('No game round exists with API identifier "%s"', $roundIdent));
+            return 2;
+        }
+
         $spec = $round->spec->getValue();
         $spec->roundId = $round->api_ident;
         $spec->roundName = $round->name;
 
-        $output->writeln(json_encode($spec), OutputInterface::VERBOSITY_VERBOSE);
+        if ($output->isVerbose()) {
+            $output->writeln(json_encode($spec));
+        } elseif ($output->isVeryVerbose()) {
+            $output->writeln(json_encode($spec, JSON_PRETTY_PRINT));
+        }
 
         $res = $this->getGatewayClient()->put('/v1/game/round', ['json' => $spec]);
+        $setupExitCode = $this->processHttpClientResult($res, $output);
+
+        if (!$input->hasOption(self::OPT_START)) {
+            return $setupExitCode;
+        }
+
+        $output->writeln('');
+        $output->writeln(sprintf('Will start in %d seconds...', self::SLEEP_BEFORE_START));
+        sleep(self::SLEEP_BEFORE_START);
+        $output->writeln('');
+
+        $res = $this->getGatewayClient()->post('/v1/game/start', [
+            'json' => [
+                'roundId' => $round->api_ident,
+                'password' => $round->api_password
+            ],
+        ]);
         return $this->processHttpClientResult($res, $output);
     }
 }
